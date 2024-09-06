2a214f4d8282b0c9242e9d784ae1745bf88ca1d0
Arslan7861291 <113829547+Arslan7861291@users.noreply.github.com>
2024-09-06 20:47:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "0iV3RRya#vkJ_gK39Z9Qj_lS6iU1qbhL1VL19kLfal_lU6qpxjj0",
+MONGODB: process.env.MONGODB || "mongodb://mongo:tcHppvmtGjEehXxhHhRLiNLDixCjRPxT@junction.proxy.rlwy.net:50567",
 };
